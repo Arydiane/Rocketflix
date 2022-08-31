8d06ef68229b12ef30cb669be12789f1d1da0009
Arydiane <103674349+Arydiane@users.noreply.github.com>
2022-08-31 15:03:07 -0300

    initial commit

---
+++ api.js
@@ -0,0 +1,5 @@
+
+export const API_KEY = 'api_key=83bab07a457c58d0c440a13153c773b7';
+export const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+export const language = 'language=pt-BR';
